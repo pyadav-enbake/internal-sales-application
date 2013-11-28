138daa0f9e01062cae857e67177ba172a72cbe80
jignesh.nakrani <jignesh.2007@gmail.com>
2013-11-28 19:02:09 +0530

    Implemented Dimension,Product Module

--- app/models/rcadmin/dimension.rb
+++ app/models/rcadmin/dimension.rb
@@ -1,17 +1,25 @@
 class Rcadmin::Dimension < ActiveRecord::Base
-	#attr_accessible :lower_range,:upper_range
 	 validates_presence_of :dimension_category_id,:lower_range
 	 validates :lower_range, numericality: true
+	 validate :upper_range_valid
 	 attr_accessible :lower_range,:upper_range,:dimension_category_id
 	 
 	belongs_to :dimension_category 
 	has_many :products,dependent: :destroy
 	
 	def fullrange
-		if(upper_range == 0)
+		if(upper_range == nil)
 			'Range '+lower_range.to_s 
 		else
 			'Range '+lower_range.to_s + ' to ' + upper_range.to_s
 		end
 	end
+	def upper_range_valid
+		if(upper_range)
+			if(upper_range < lower_range)
+				errors.add(:base, "Upper range should greater than lower range")
+			end
+		end
+	end
+	
 end

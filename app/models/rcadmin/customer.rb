0683fd2c8c1a4c4e1b401d23749222936a28e08f
jignesh.nakrani <jignesh.2007@gmail.com>
2013-12-12 10:36:18 +0530

    Fixed validation

--- app/models/rcadmin/customer.rb
+++ app/models/rcadmin/customer.rb
@@ -1,5 +1,5 @@
 class Rcadmin::Customer < ActiveRecord::Base
-	validates_presence_of :admin_id,:first_name,:last_name,:email,:state,:city,:address,:zip,,:phone,:status
+	validates_presence_of :admin_id,:first_name,:last_name,:email,:state,:city,:address,:zip,:phone,:status
 	attr_accessible :admin_id,:first_name,:last_name,:email,:state,:city,:address,:zip,:phone,:status
 	
 	belongs_to :admin

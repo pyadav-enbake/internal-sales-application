6ce1ce6c41076a6e40b6c7d70334a51e827d84d7
jignesh.nakrani <jignesh.2007@gmail.com>
2013-12-12 10:33:45 +0530

    Fixed validation

--- app/models/rcadmin/customer.rb
+++ app/models/rcadmin/customer.rb
@@ -1,6 +1,6 @@
 class Rcadmin::Customer < ActiveRecord::Base
 	validates_presence_of :admin_id,:first_name,:last_name,:email,:state,:city,:address,:zip,,:phone,:status
-	attr_accessible :admin_id,:first_name,:last_name,:email,:state,:city,:address,:zip,,:phone,:status
+	attr_accessible :admin_id,:first_name,:last_name,:email,:state,:city,:address,:zip,:phone,:status
 	
 	belongs_to :admin
 	scope :default, -> {  where(:status => 0)  }

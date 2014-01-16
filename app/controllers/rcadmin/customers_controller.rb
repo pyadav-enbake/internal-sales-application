ebda1dc5536d74e7c5e263360c92d39d650fc0fe
akankshita <akankshita009@gmail.com>
2014-01-16 19:33:08 +0530

    Contractor

--- app/controllers/rcadmin/customers_controller.rb
+++ app/controllers/rcadmin/customers_controller.rb
@@ -16,7 +16,6 @@ class Rcadmin::CustomersController < ApplicationController
   # GET /rcadmin/customers/new
   def new
     @rcadmin_customer = Rcadmin::Customer.new
-    render :text => @rcadmin_customer.inspect and return false
   end
 
   # GET /rcadmin/customers/1/edit

23dde17a9c7be93f9642f488b53d6fe8313b0d0a
vivek varade <varade.vivek.123@gmail.com>
2014-02-03 10:06:40 +0530

    All checkbox added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,66 +6,77 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
  # before_filter :authenticate
   
-  layout Proc.new { |controller| controller.devise_controller? ? 'devise' : 'application' }
-	#WelcomeMailer.testmail().deliver
-	def configure_permitted_parameters
-	  devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:name, :email) }
-	  devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation,:first_name,:last_name,:terms_and_conditions,:role) }
-	  devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation,:first_name,:last_name,:role,:quote_category) }
-	end
-  
-  def after_sign_in_path_for(resource)
-	@current_user = Rcadmin::Admin.find(resource[:id])
-    session[:username] = resource[:username]
-    session[:email] = resource[:email]
-    session[:admin_id] = resource[:id]
-    session[:first_name] = resource[:first_name]
-    session[:last_name] = resource[:last_name]
+  layout :get_layout 
 
-	@loginlogs = Rcadmin::LoginLog.new
-	@loginlogs.email = resource[:email]
-	@loginlogs.login_time = Time.now
-	@loginlogs.first_name = resource[:first_name]
-	@loginlogs.last_name = resource[:last_name]
-	@loginlogs.logout_time = ""
-	@loginlogs.ip = request.remote_ip
-	@loginlogs.role = current_user.role
-	@loginlogs.save
-	session[:login_log_id] = @loginlogs.id    
-    
-	dashboard_url_path
+  def get_layout
+      if controller_name == 'devise'
+          'devise'
+      elsif controller_name == 'quote' && action_name == 'quote_preview'
+          'mail_preview'
+      else 
+          'application'
+      end
+  end
+
+  #WelcomeMailer.testmail().deliver
+  def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:name, :email) }
+      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation,:first_name,:last_name,:terms_and_conditions,:role) }
+      devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation,:first_name,:last_name,:role,:quote_category) }
+  end
+
+  def after_sign_in_path_for(resource)
+      @current_user = Rcadmin::Admin.find(resource[:id])
+      session[:username] = resource[:username]
+      session[:email] = resource[:email]
+      session[:admin_id] = resource[:id]
+      session[:first_name] = resource[:first_name]
+      session[:last_name] = resource[:last_name]
+
+      @loginlogs = Rcadmin::LoginLog.new
+      @loginlogs.email = resource[:email]
+      @loginlogs.login_time = Time.now
+      @loginlogs.first_name = resource[:first_name]
+      @loginlogs.last_name = resource[:last_name]
+      @loginlogs.logout_time = ""
+      @loginlogs.ip = request.remote_ip
+      @loginlogs.role = current_user.role
+      @loginlogs.save
+      session[:login_log_id] = @loginlogs.id    
+
+      dashboard_url_path
   end
   def after_sign_up_path_for(resource)
-	new_admin_session_path
+      new_admin_session_path
   end
-  
+
 
   def check_auth
-  
-	if !admin_signed_in?
-		redirect_to "/"
-	end
+
+      if !admin_signed_in?
+          redirect_to "/"
+      end
   end
 
   def authenticate
-  	if current_user
-#render :text => params.inspect and return false
-		if current_user.role == 'sales_admin' && (params[:controller] != 'rcadmin/customers' && params[:controller] != 'rcadmin/dashboard') &&( params[:controller] == 'rcadmin/administrators' && current_user.id != params[:id].to_i)
-			redirect_to "/error404"
-		end
-		#if current_user.role == 'admin' && params[:controller] == 'rcadmin/customers'
-		#	redirect_to "/error404"
-		#end
-	end
+      if current_user
+          #render :text => params.inspect and return false
+          if current_user.role == 'sales_admin' && (params[:controller] != 'rcadmin/customers' && params[:controller] != 'rcadmin/dashboard') &&( params[:controller] == 'rcadmin/administrators' && current_user.id != params[:id].to_i)
+              redirect_to "/error404"
+          end
+          #if current_user.role == 'admin' && params[:controller] == 'rcadmin/customers'
+          #	redirect_to "/error404"
+          #end
+      end
   end
-  
+
   private
-  
+
   def current_user
- # render :text =>params[:controller] and return false
-	if admin_signed_in?
-	@current_user = Rcadmin::Admin.find(session[:admin_id])
-	end
+      # render :text =>params[:controller] and return false
+      if admin_signed_in?
+          @current_user = Rcadmin::Admin.find(session[:admin_id])
+      end
   end
 
 

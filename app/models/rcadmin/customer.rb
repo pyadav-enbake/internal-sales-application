f18cc6c775e772d95ec7a9095112cfbcd00e8ab7
Ismail Akram <rubyonrails3@gmail.com>
2014-02-13 16:38:41 +0500

    Changed state on form when creating customer

--- app/models/rcadmin/customer.rb
+++ app/models/rcadmin/customer.rb
@@ -1,8 +1,8 @@
 class Rcadmin::Customer < ActiveRecord::Base
-  validates_presence_of :contractor_id,:first_name,:last_name,:email
+  validates_presence_of :first_name, :last_name, :email
   validates :email, :presence => true, :email => true
   validates_uniqueness_of :email,:on => :create
-  validates_presence_of :state,:city,:address,:zip
+  validates_presence_of :state, :city, :address, :zip
   validates_format_of :zip, :with => /^([0-9]){5}(-([0-9]){4})?$/,:multiline => true, :message => "should be in the form 12345 or 12345-1234"
   validates_presence_of:phone,:status
   validates :phone, :length => {:minimum => 6, :maximum => 25}, :format => { :with => /\A\S[0-9\+\/\(\)\s\-]*\z/i }

36d9e6bf48144b631a268c7c98b537edc6fb61ea
Ismail Akram <rubyonrails3@gmail.com>
2014-05-10 00:21:51 +0500

    Fixed wrong amount of quote products

--- app/models/rcadmin/quote_category.rb
+++ app/models/rcadmin/quote_category.rb
@@ -4,7 +4,9 @@ class Rcadmin::QuoteCategory < ActiveRecord::Base
   belongs_to :quote
   belongs_to :category
 
-  has_many :quote_products, -> { extending ProductTypeExtension}, foreign_key: :category_id, primary_key: :category_id
+  has_many :quote_products, -> (object){ where(quote_id: object.quote_id) }, foreign_key: :category_id do
+    include ProductTypeExtension
+  end
 
   before_save do
     self.markup = 0 if markup.nil?

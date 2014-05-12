64e8913656c2c5dc24039f70a7d87992dcdad98b
Ismail Akram <rubyonrails3@gmail.com>
2014-05-12 22:32:34 +0500

    Refined OPTIONS and Grand Calculator

--- app/models/rcadmin/quote_product.rb
+++ app/models/rcadmin/quote_product.rb
@@ -7,11 +7,17 @@ class  Rcadmin::QuoteProduct < ActiveRecord::Base
   has_one :quote_category, -> (object) { where(quote_id: object.quote_id) },
     foreign_key: :category_id, primary_key: :category_id
 
+  delegate :factor, :percentage, to: :quote_category
+
   belongs_to :product, polymorphic: true
 
   scope :find_by_quote, ->(quote_id) { where(:quote_id=>quote_id) }
 
   scope :has_option, ->(value) { where(header_option: value) }
+
+  def options_total_price
+    (self.total_price * (percentage/100) * factor).round(2)
+  end
 
 
 
